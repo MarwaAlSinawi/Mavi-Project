892fdb6dfc2a2db48432c19894881aacf7553eca
MarwaAlSinawi <116248656+MarwaAlSinawi@users.noreply.github.com>
2023-01-15 16:51:49 +0400

    MAVENProject

---
+++ src/main/java/apiM/ImportingApi.java
@@ -0,0 +1,90 @@
+package apiM;
+
+public class ImportingApi {
+
+	private String state_province;
+	private String alpha_two_code;
+	private String name;
+	private String country;
+	private String[] domains;
+	private String[] web_pages;
+	/**
+	 * @return the state_province
+	 */
+	public String getState_province() {
+		return state_province;
+	}
+	/**
+	 * @param state_province the state_province to set
+	 */
+	public void setState_province(String state_province) {
+		this.state_province = state_province;
+	}
+	/**
+	 * @return the alpha_two_code
+	 */
+	public String getAlpha_two_code() {
+		return alpha_two_code;
+	}
+	/**
+	 * @param alpha_two_code the alpha_two_code to set
+	 */
+	public void setAlpha_two_code(String alpha_two_code) {
+		this.alpha_two_code = alpha_two_code;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	/**
+	 * @return the domains
+	 */
+	public String[] getDomains() {
+		return domains;
+	}
+	/**
+	 * @param domains the domains to set
+	 */
+	public void setDomains(String[] domains) {
+		this.domains = domains;
+	}
+	/**
+	 * @return the web_pages
+	 */
+	public String[] getWeb_pages() {
+		return web_pages;
+	}
+	/**
+	 * @param web_pages the web_pages to set
+	 */
+	public void setWeb_pages(String[] web_pages) {
+		this.web_pages = web_pages;
+	}
+
+
+
+
+	
+	
+	
+}
